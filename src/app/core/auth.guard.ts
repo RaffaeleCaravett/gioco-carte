7ef240b1aa37ad4e230356bd6033961829f4e3b0
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-01-09 21:32:09 +0100

    developing login component

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,5 +1,36 @@
-import type { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import type { ActivatedRouteSnapshot, CanActivate,  RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private a :AuthService) {}
+
+
+  isAuthenticated:boolean = false;
+
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+
+    if (this.isAuthenticated) {
+      return true;
+    } else {
+      this.a.navigateHome()
+      return false;
+    }
+  }
+  authenticateUser(bool?:boolean){
+  if(bool){
+     this.isAuthenticated=bool
+  }else{
+    this.isAuthenticated=false
+  }
+}
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
+
+navigateHome(){
+ return this.router.navigate(['/home'])
+}
 
 }
